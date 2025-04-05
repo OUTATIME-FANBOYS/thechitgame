52bfac647011a4cb7d418915a62eff0a0e207e14
Tanmay Dureja <tanmaydureja@gmail.com>
2025-04-05 13:03:04 -0700

    Initial Commit

---
+++ PartyPromptApp/PartyPromptAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyPromptAppApp.swift
+//  PartyPromptApp
+//
+//  Created by Tanmay Dureja on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PartyPromptAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
