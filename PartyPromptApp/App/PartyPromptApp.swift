a636ee36555b0bbf736afc8bb29e545efc01f8c7
Tanmay Dureja <tanmaydureja@gmail.com>
2025-07-13 14:19:26 -0700

    Initiate Firebase

--- PartyPromptApp/App/PartyPromptApp.swift
+++ PartyPromptApp/App/PartyPromptApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Firebase
 
 @main
 struct PartyPromptApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
